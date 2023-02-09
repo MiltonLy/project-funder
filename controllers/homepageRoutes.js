0ef1212df243c66366374c9a4bc0944254acc82c
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2023-02-08 19:56:45 -0800

    project Routes

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -19,24 +19,42 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/project/:id', async (req, res) => {
-  try {
-    const project = await Project.findByPk({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.render('', { project });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+router.get('/project/:id', withAuth, async (req, res) => {
+    try {
+        const project = await Project.findByPk(req.params.id)
+        // project by ID
+        res.render('', {project});
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err)
+    }
 });
 
 // It's done when the /profile route renders the logged-in user's projects and a form to create a new project.
-router.get('/project', withAuth, async (req, res) => {});
+router.get('/project', withAuth, async (req, res) => {
+    try {
+        // matching user email with db
+        const userProjects = await Project.findAll({
+            where: {
+                // is this right?
+                user_id: req.session.user_id
+            },
+        })
+        const projects = userProjects.map((project) => project.get({ plain:true }));
+        // projects by user
+        res.render('project', { projects });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+});
 
 router.get('/login', (req, res) => {
-  res.render('login');
+    if (req.session.loggedIn) {
+        res.redirect('/login');
+        return;
+    }
+    res.render('login')
 });
+
 module.exports = router;
